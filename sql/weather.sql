0fc2e9af74df32394da45b9e70024635978a1c11
Dusan Chromy <dusan.chromy@yahoo.de>
2018-01-30 15:45:29 +0100

    Get all possible fields from JSON

--- sql/weather.sql
+++ sql/weather.sql
@@ -1,5 +1,12 @@
+-- drop table weather;
 create table weather (
+        `coord.lon` decimal(4,2),
+        `coord.lat` decimal(4,2),
         base varchar(128),
+        `main.temp` decimal(5,2),
+        `wind.speed` decimal(5,2),
+        `wind.deg` integer,
+        `clouds.all` integer,
         visibility integer,
         dt integer,
         id integer,
